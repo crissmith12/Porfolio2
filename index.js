2e3b698e282f19746a45943f4b33075339ec32a4
crissmith12 <155885550+crissmith12@users.noreply.github.com>
2024-04-13 21:09:01 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// let greetings = 'hey'
+
+// console.log(greetings);
+
+// let person = {
+//     name: 'crissmith',
+//     age: 24
+// }; //dictionary
+// //dot notation if you want to change
+// person.name = 'mariecar'
+
+// console.log(person.name);
+
+
+// // let firstName = undefined;
+// // let lastName = null;
+// // let disApproved = false/true; //boonlean literal
+// // let name = 'ANY'; //string literal
+let age = 18; //Number literal
+// // let selectedColor = null;
+// // console.log(firstName)
+
+// //var/Let use for changable character
+// //const cannot change
+// //comsole.log output syntax
+
+// let color ='Red'
+// console.log(color)
+// //arrays
+// let selectedColors = ['red', 'blue' ];
+// selectedColors[2] = 'green';
+
+// console.log(selectedColors)
+
+// let greeting ='hi'
+// console.log(greeting);
+
+
+if(age >= 18){
+    console.log('Legal age')
+}
+
+else if(age >=5){
+    console.log('minor')
+}
+
+// // }
+
+// // let name = prompt("Whats your name?");
+
+// // console.log(name)
+
+let number = 15;
+
+if (number >=13){
+    console.log ('yes sir')
+}else {
+
+    console.log ('no')
+}
+
+
+
